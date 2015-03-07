d11e2b32e65255ab586c214e15046365a0f75f52
sangm <sang.mercado@gmail.com>
2015-03-07 06:44:15 -0600

    Front end nearly done

--- lib/components/HeaderComp.jsx
+++ lib/components/HeaderComp.jsx
@@ -7,16 +7,17 @@ var RaisedButton = mui.RaisedButton;
 
 export default React.createClass({
 
-  render() {
-    let twitterStyle = { color: '#3498db' };
-    return (
-        <div className="container-fluid">
-            <header className="text-center">
-                <i style={twitterStyle} className="fa fa-twitter fa-3x"></i>
-            </header>
-            <hr/>
-        </div>
-    );
-  }
+    render() {
+        let twitterStyle = { color: '#3498db' };
+        let headerStyle = { borderBottom: '1px solid #eee' };
+        return (
+            <div>
+                <header className="container-fluid text-center"
+                        style={headerStyle}>
+                    <i style={twitterStyle} className="fa fa-twitter fa-3x"></i>
+                </header>
+            </div>
+        );
+    }
 
 });
--- lib/components/Home.jsx
+++ lib/components/Home.jsx
@@ -3,20 +3,28 @@
 import React from 'react';
 import { RouteHandler } from 'react-router';
 import Header from './HeaderComp.jsx';
+import Stats from './StatsComp.jsx';
+import Searchbar from './SearchbarComp.jsx';
+import SideFacet from './SideFacetComp.jsx';
 import Footer from './FooterComp.jsx';
 
 export default React.createClass({
 
-  render() {
-    return (
-      <div>
-        <Header/>
-        <main>
-          <RouteHandler/>
-        </main>
-        <Footer/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header/>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-offset-2 col-md-8 col-xs-12">
+                            <Stats />
+                            <Searchbar />
+                            <SideFacet />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
 });
